ec2e8f6167b2cc4b4ee107a9deb5d48fe6a333bf
Atul Pahuja <apahuja@sapient.com>
2018-05-08 17:39:18 +0530

    websocket event connection and disconnect event

--- server/server.js
+++ server/server.js
@@ -1,11 +1,34 @@
 const path = require('path');
-const publicPath = path.join(__dirname, "../public");
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
+
+const publicPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen( port, ()=>{
-    console.log(`Server is started on port ${port}`);
+io.on('connection',(socket)=>{
+console.log('New User connected');
+})
+
+io.on('disconnect',()=>{
+    console.log('disconnect');
+})
+
+server.listen(port, ()=>{
+    console.log("Server is started on port :: ", port);
 });
+
+
+
+
+
+
+
+
+
+
